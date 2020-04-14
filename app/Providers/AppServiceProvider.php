74f8f9bc623e5c9fe31730282459247152f18712
kyle <Kyle>
2020-04-14 13:09:04 -0500

    fix for migration error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //FIX per documentation to solve mysql migration error
     }
 }
